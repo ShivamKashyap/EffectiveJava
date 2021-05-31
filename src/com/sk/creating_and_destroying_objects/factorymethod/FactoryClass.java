3644a9430d3313cfbf8b55f2b96be371609710a7
Shivam Kashyap <kashyapshivam92@gmail.com>
2021-05-31 13:24:07 +0530

    Advantages and Limitations of Static factory method added to class doc

--- src/com/sk/creating_and_destroying_objects/factorymethod/FactoryClass.java
+++ src/com/sk/creating_and_destroying_objects/factorymethod/FactoryClass.java
@@ -1,5 +1,21 @@
 package com.sk.creating_and_destroying_objects.factorymethod;
 
+/**
+ * 
+ * @author Shivam Kashyap
+ * 
+ * Advantages of static factory methods:
+ * 1. Unlike Constructors, static factory method have names
+ * 2. Unlike Constructors, static factory methods need not  required to create a new object each time they are invoked(Restrictive Instance).
+ * 3. Unlike Constructors, static factory methods can return an object of any sub type of their return type
+ * 4. In Static factory methods, class of the returned object can vary from call to call as function of the input parameter.
+ * 5.  In Static factory methods, class of the returned object need not exist  when the class containing the method is return.
+ * 
+ * LIMITATION of Static factory method:
+ * 1. static factory methods is that classes without public or protected constructors cannot be sub classed.
+ * 2. Static factory method  are hard to for programmers to find (programmers have to refer java doc)
+ *
+ */
 class FactoryClass {
 
 	private String field;
